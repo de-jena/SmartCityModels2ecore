a0a72eee8377d6e779bab0cd3695a0529f533785
Ilenia Salvadori <i.salvadori@data-in-motion.biz>
2022-07-22 06:23:06 +0200

    added org.gecko.emf.json snapshot to support type package
    Signed-off-by: Ilenia Salvadori <i.salvadori@data-in-motion.biz>

--- org.gecko.smartmodels.tests/src/org/gecko/smartmodels/tests/EcoreConcreteObjectConverterImpl.java
+++ org.gecko.smartmodels.tests/src/org/gecko/smartmodels/tests/EcoreConcreteObjectConverterImpl.java
@@ -12,18 +12,22 @@
 package org.gecko.smartmodels.tests;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EPackage.Registry;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.emfjson.jackson.databind.EMFContext;
+import org.eclipse.emfcloud.jackson.annotations.EcoreTypeInfo;
+import org.eclipse.emfcloud.jackson.databind.EMFContext;
+import org.gecko.emf.json.constants.EMFJs;
 import org.gecko.smartmodels.apis.ecore.EcoreConcreteObjectConverter;
 import org.gecko.smartmodels.building.model.building.Building;
+import org.gecko.smartmodels.building.model.building.BuildingPackage;
+import org.gecko.smartmodels.geojson.model.geojson.GeojsonPackage;
 import org.gecko.smartmodels.geojson.model.geojson.Polygon;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -51,15 +55,17 @@ public class EcoreConcreteObjectConverterImpl implements EcoreConcreteObjectConv
 		try {		
 			Map<Object, Object> loadOptions = new HashMap<Object, Object>();
 			loadOptions.put(EMFContext.Attributes.ROOT_ELEMENT, eclass);
+			loadOptions.put(EMFJs.OPTION_TYPE_USE, EcoreTypeInfo.USE.NAME);
+			loadOptions.put(EMFJs.OPTION_TYPE_FIELD, "type");
+			loadOptions.put(EMFJs.OPTION_TYPE_PACKAGE_URIS, List.of(GeojsonPackage.eNS_URI, BuildingPackage.eNS_URI));
+			URI uri = URI.createURI(GeojsonPackage.eNS_URI);
+			System.out.println("URI " + uri);
+			
 			inRes.load(loadOptions);
 			if(inRes.getContents() != null && inRes.getContents().size() > 0) {
 				if(inRes.getContents().get(0).eClass().equals(eclass)) {
 					System.out.println("Classes are equals!!");
 					System.out.println(inRes.getContents().get(0));
-					EObject mainObj = (EObject) inRes.getContents().get(0);
-					for(EObject cont : mainObj.eContents()) {
-						System.out.println(cont);
-					}
 					Building building = (Building) inRes.getContents().get(0);
 					System.out.println("Location " + building.getLocation());
 					if(building.getLocation() instanceof Polygon) {
